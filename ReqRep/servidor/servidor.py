7bbf8d0b247d6814a76ac17ff342f3366e177c64
PCunh <160776541+PCunh@users.noreply.github.com>
2026-09-05 01:24:25 +0000

    ReqRep completo

---
+++ ReqRep/servidor/servidor.py
@@ -0,0 +1,46 @@
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.connect("tcp://broker:5556")
+
+tarefas = []
+
+while True:
+    message = socket.recv()
+    
+    partes = mensagem.split(" ", 1)
+    comando = partes[0].upper()
+
+    if comando ==  "ADD":
+        if len(partes) < 2:
+            resposta = "Erro: informe tarefa"
+        else:
+            tarefas.append(partes[1])
+            resposta = "Tarefa adicionada: {partes[1]}"
+
+    elif comando == "LIST":
+        if not tarefas:
+            resposta = "Neca de pitibiriba"
+        else:
+            resposta = "\n".join(
+                f"{i+1}. {t}" for i, t in enumerate(tarefas)
+            )
+    elif comando == "REMOVE":
+        if len(partes) < 2:
+            resposta = "Erro: informe numero "
+        else:
+            try:
+                indice = int(partes[1]) - 1
+
+                if 0 <= indice < len(tarefas):
+                    removida = tarefas.pop(indice)
+                    resposta = f"Removida: {removida}"        
+                else:
+                    resposta = "ìndice inválido"
+            except ValueError:
+                resposta = "Digite um número"
+
+        else:
+            resposta = "Comando inválido"
+        socket.send_string(resposta)
